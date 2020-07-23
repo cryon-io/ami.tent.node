a67e8e564f529a7ac670e7f4332e2e8ba6f16708
cryi <cryi@tutanota.com>
2020-07-23 20:58:08 +0200

    leverage ami 0.1.6 object like tables

--- src/model.ext.lua
+++ src/model.ext.lua
@@ -14,10 +14,15 @@ local _rpcPass = eliUtil.random_string(20, _charsetTable)
 APP.model = eliUtil.merge_tables(
     APP.model,
     {
-        RPC_USER = APP.user,
-        RPC_PASS = APP.configuration.RPC_PASS or _rpcPass,
-        RPC_PORT = APP.configuration.RPC_PORT or 16111,
-        IS_SERVER = type(APP.configuration.NODE_PRIVKEY) == 'string' or APP.configuration.SERVER,
+        DAEMON_CONFIGURATION = {
+            rpcuser = APP.configuration.RPC_USER or APP.user,
+            rpcpassword = APP.configuration.RPC_PASS or _rpcPass,
+            rpcport = APP.configuration.RPC_PORT or 16111,
+            server = (type(APP.configuration.NODE_PRIVKEY) == 'string' or APP.configuration.SERVER) and 1 or nil,
+            listen = (type(APP.configuration.NODE_PRIVKEY) == 'string' or APP.configuration.SERVER) and 1 or nil,
+            masternodeprivkey = configuration.NODE_PRIVKEY,
+            masternode = configuration.NODE_PRIVKEY and 1,
+        },
         DAEMON_NAME = "snowgemd",
         CLI_NAME = "snowgem-cli",
         CONF_NAME = "snowgem.conf",
