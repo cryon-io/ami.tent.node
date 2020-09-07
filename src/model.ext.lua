7085a7d60b5f18fa77c6df57f4c7e6a65cc7a452
cryi <cryi@tutanota.com>
2020-09-07 19:34:55 +0200

    fixed automatic rpcuser/rpcpassword

--- src/model.ext.lua
+++ src/model.ext.lua
@@ -2,26 +2,42 @@ if type(APP.model) ~= "table" then
     APP.model = {}
 end
 
-if type(APP.configuration) ~= 'table' then
-    ami_error("Configuration not found...", EXIT_INVALID_CONFIGURATION) 
+if type(APP.configuration) ~= "table" then
+    ami_error("Configuration not found...", EXIT_INVALID_CONFIGURATION)
 end
 
 local _charsetTable = {}
-_charset="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-_charset:gsub(".",function(c) table.insert(_charsetTable,c) end)
+_charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+_charset:gsub(
+    ".",
+    function(c)
+        table.insert(_charsetTable, c)
+    end
+)
 local _rpcPass = eliUtil.random_string(20, _charsetTable)
 
-APP.model = eliUtil.merge_tables(
+local _daemonConfiguration = {}
+if type(APP.configuration.DAEMON_CONFIGURATION) ~= "table" or not APP.configuration.DAEMON_CONFIGURATION.rpcuser then
+    _daemonConfiguration.rpcuser = APP.user
+end
+
+if type(APP.configuration.DAEMON_CONFIGURATION) ~= "table" or not APP.configuration.DAEMON_CONFIGURATION.rpcpassword then
+    _daemonConfiguration.rpcpassword = _rpcPass
+end
+
+APP.model =
+    eliUtil.merge_tables(
     APP.model,
     {
-        DAEMON_CONFIGURATION = {
-            rpcuser = APP.configuration.RPC_USER or APP.user,
-            rpcpassword = APP.configuration.RPC_PASS or _rpcPass,
-            server = (type(APP.configuration.NODE_PRIVKEY) == 'string' or APP.configuration.SERVER) and 1 or nil,
-            listen = (type(APP.configuration.NODE_PRIVKEY) == 'string' or APP.configuration.SERVER) and 1 or nil,
-            masternodeprivkey = APP.configuration.NODE_PRIVKEY,
-            masternode = APP.configuration.NODE_PRIVKEY and 1,
-        },
+        DAEMON_CONFIGURATION = eliUtil.merge_tables(
+            {
+                server = (type(APP.configuration.NODE_PRIVKEY) == "string" or APP.configuration.SERVER) and 1 or nil,
+                listen = (type(APP.configuration.NODE_PRIVKEY) == "string" or APP.configuration.SERVER) and 1 or nil,
+                masternodeprivkey = APP.configuration.NODE_PRIVKEY,
+                masternode = APP.configuration.NODE_PRIVKEY and 1
+            },
+            _daemonConfiguration
+        ),
         DAEMON_NAME = "snowgemd",
         CLI_NAME = "snowgem-cli",
         CONF_NAME = "snowgem.conf",
@@ -30,5 +46,6 @@ APP.model = eliUtil.merge_tables(
         MN_CONF_SOURCE = "__xsg/assets/masternode.conf",
         SERVICE_NAME = "snowgemd",
         DATA_DIR = "data"
-    }, true
+    },
+    true
 )
