fc66e310fcaeeaca3f34e888a057ee4864dd8d63
cryi <cryi@tutanota.com>
2020-12-05 11:45:13 +0100

    updated to leverage btc.base 0.2.0

--- src/model.ext.lua
+++ src/model.ext.lua
@@ -1,30 +1,3 @@
-if type(APP.model) ~= "table" then
-    APP.model = {}
-end
-
-if type(APP.configuration) ~= "table" then
-    ami_error("Configuration not found...", EXIT_INVALID_CONFIGURATION)
-end
-
-local _charsetTable = {}
-_charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-_charset:gsub(
-    ".",
-    function(c)
-        table.insert(_charsetTable, c)
-    end
-)
-local _rpcPass = eliUtil.random_string(20, _charsetTable)
-
-local _daemonConfiguration = {}
-if type(APP.configuration.DAEMON_CONFIGURATION) ~= "table" or not APP.configuration.DAEMON_CONFIGURATION.rpcuser then
-    _daemonConfiguration.rpcuser = APP.user
-end
-
-if type(APP.configuration.DAEMON_CONFIGURATION) ~= "table" or not APP.configuration.DAEMON_CONFIGURATION.rpcpassword then
-    _daemonConfiguration.rpcpassword = _rpcPass
-end
-
 APP.model =
     eliUtil.merge_tables(
     APP.model,
