7ffd630828ae7c99860ba9d29d282106ae2ce99b
cryi <cryi@tutanota.com>
2020-12-26 00:29:57 +0100

    additional info improvements

--- src/__tent/info.lua
+++ src/__tent/info.lua
@@ -1,22 +1,22 @@
 local _json = ...
-local _hjson = require"hjson"
+local _hjson = require "hjson"
 
 local _ok, _systemctl = safe_load_plugin("systemctl")
 ami_assert(_ok, "Failed to load systemctl plugin", EXIT_PLUGIN_LOAD_ERROR)
 
-local _info = {}
 local _ok, _status, _started = _systemctl.safe_get_service_status(APP.id .. "-" .. APP.model.SERVICE_NAME)
 ami_assert(_ok, "Failed to start " .. APP.id .. "-" .. APP.model.SERVICE_NAME .. ".service " .. (_status or ""), EXIT_PLUGIN_EXEC_ERROR)
-_info.snowgemd = _status
-_info.started = _started
 
-_info.level = "ok"
-_info.synced = "not reported"
-_info.status = "XSG node down"
+local _info = {
+    snowgemd = _status,
+    started = _started,
+    level = "ok",
+    synced = "not reported",
+    status = "XSG node down"
+}
 
 local function _exec_xsg_cli(...)
-
-    local arg = { "-datadir=data", ... }
+    local arg = {"-datadir=data", ...}
     if type(APP.configuration.DAEMON_CONFIGURATION) == "table" and type(APP.configuration.DAEMON_CONFIGURATION.rpcbind) == "string" then
         table.insert(arg, 1, "-rpcconnect=" .. APP.configuration.DAEMON_CONFIGURATION.rpcbind)
     end
@@ -45,17 +45,15 @@ local function _get_xsg_cli_result(exitcode, stdout, stderr)
         local _ok, _output = pcall(_hjson.parse, _errorInfo)
         if _ok then
             return false, _output
-        else
-            return false, { message = "unknown (internal error)" }
         end
+        return false, {message = "unknown (internal error)"}
     end
 
     local _ok, _output = pcall(_hjson.parse, stdout)
     if _ok then
         return true, _output
-    else
-        return false, { message = "unknown (internal error)" }
     end
+    return false, {message = "unknown (internal error)"}
 end
 
 local function _update_info(update_function)
@@ -67,20 +65,17 @@ end
 
 if _info.snowgemd == "running" then
     local _checks = {
-        function () -- blockchain info check
+        function()
+            -- blockchain info check
             local _exitcode, _stdout, _stderr = _exec_xsg_cli("getblockchaininfo")
             local _success, _output = _get_xsg_cli_result(_exitcode, _stdout, _stderr)
 
-            if _success then
-                _info.currentBlock = _output.blocks
-                _info.currentBlockHash = _output.bestblockhash
-            else
-                _info.currentBlock = "unknown"
-                _info.currentBlockHash = "unknown"
-            end
+            _info.currentBlock = _success and _output.blocks or "unknown"
+            _info.currentBlockHash = _success and _output.bestblockhash or "unknown"
         end,
-        function () -- synchronization check
-            local _exitcode, _stdout, stderr = _exec_xsg_cli("getblockchainsyncstatus")
+        function()
+            -- synchronization check
+            local _exitcode, _stdout, _stderr = _exec_xsg_cli("getblockchainsyncstatus")
             local _success, _output = _get_xsg_cli_result(_exitcode, _stdout, _stderr)
 
             if _success then
@@ -90,38 +85,45 @@ if _info.snowgemd == "running" then
                     _info.level = "warn"
                     _info.status = "Syncing..."
                 end
-            elseif _stderr:match('Snowgem is not connected!') then
-                _info.level = "warn"
-                _info.status = "Snowgem is not connected!"
-            elseif _stderr:match('Snowgem is downloading blocks...') then
-                _info.level = "warn"
-                _info.status = "Snowgem is downloading blocks..."
-            else
-                _info.level = "warn"
-                _info.status = "Unknown error..."
+                return
             end
+
+            if type(_stderr) ~= "string" then
+                _stderr = ""
+            end
+
+            _info.level = "warn"
+            _info.status = _stderr:match("Snowgem is not connected!") or
+                _stderr:match("Snowgem is downloading blocks...") or
+                "Unknown error..."
         end,
-        function () -- masternode status check
+        function()
+            -- masternode status check
             if type(APP.configuration.DAEMON_CONFIGURATION) == "table" and tonumber(APP.configuration.DAEMON_CONFIGURATION.masternode) == 1 then
-                local _exitcode, _stdout, _stderr = _exec_xsg_cli("masternode", "debug")
+                local _, _stdout, _stderr = _exec_xsg_cli("masternode", "debug")
                 if type(_stdout) ~= "string" then
                     _stdout = ""
                 end
+
+                if _stdout:match("Masternode successfully started") then
+                    _info.status = "Masternode successfully started"
+                    return
+                end
+
                 if type(_stderr) ~= "string" then
                     _stderr = ""
                 end
-                if _stdout:match('Masternode successfully started') then
-                    _info.status = "Masternode successfully started"
-                elseif _stdout:match('Hot node, waiting for remote activation') or _stderr:match('Hot node, waiting for remote activation') then
-                    _info.level = "warn"
-                    _info.status = 'Hot node, waiting for remote activation.'
-                elseif _stdout:match('Node just started, not yet activated') or _stderr:match('Node just started, not yet activated') then
-                    _info.level = "warn"
-                    _info.status = 'Node just started, not yet activated'
-                else
-                    _info.level = "error"
-                    _info.status = _stderr:match("error message:.-\n(.-)\n%s*") or _stdout:match("error message:.-\n(.-)\n%s*") or "Failed to verify masternode status!"
-                end
+
+                local _warnMsg = _stdout:match("Hot node, waiting for remote activation") or
+                    _stderr:match("Hot node, waiting for remote activation") or
+                    _stdout:match("Node just started, not yet activated") or
+                    _stderr:match("Node just started, not yet activated")
+
+                _info.level = _warnMsg and "warn" or "error"
+                _info.status = _warnMsg or
+                    _stderr:match("error message:.-\n(.-)\n%s*") or
+                    _stdout:match("error message:.-\n(.-)\n%s*") or
+                    "Failed to verify masternode status!"
             else
                 _info.status = "XSG node up"
             end
@@ -140,7 +142,7 @@ _info.version = get_app_version()
 _info.type = APP.type.id
 
 if _json then
-   print(_hjson.stringify_to_json(_info, { indent = false }))
+    print(_hjson.stringify_to_json(_info, {indent = false}))
 else
-   print(_hjson.stringify(_info))
+    print(_hjson.stringify(_info))
 end
